513d8299b49fa0daaee75bcb8a5bc60281fd8d29
Muhdsodiq Bolarinwa <105107754+Amity808@users.noreply.github.com>
2023-06-24 08:07:15 +0000

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import { store } from './services/Store.js'
+import { store } from '../src/services/store.js'
 
 
 // we have to wrap our application into the provider
